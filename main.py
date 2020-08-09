d9c034d877e6091e09862c9c219142dcbdc663b2
Jiri Mares <marj39@vse.cz>
2020-08-09 15:31:19 +0200

    generating a file, generating number of attacks to a file

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-from src.generate import generate
+from src.generate import generate, generate_a_file, generate_with_file
 
 while True:
-    cmd = input("Available commands: 's' for start, 'q' for quit.\n")
+    cmd = input("Available commands: 'g' for generate, 'gg' for generate in a file, 'o' to generate an output file, 'q' for quit.\n")
 
-    if cmd == "s":
+    if cmd == "g":
         print(generate())
+
+    elif cmd == "gg":
+        filename = input("Type in the filename:\n")
+        n_of_attacks = input("Type in the number of attacks to generate:\n")
+        generate_with_file(filename, n_of_attacks)
+        print("Done.")
+
+    elif cmd == "h":
+        print("help")
+
+    elif cmd == "i":
+        print("input")
+
+    elif cmd == "o":
+        filename = input("Type in the filename:\n")
+        n_of_lines = input("Type in the number of lines to generate:\n")
+        generate_a_file(filename, n_of_lines)
+        print("Done.")
 
     elif cmd == "q":
         print("quit")
         break
+
+    else:
+        print("wrong command")
--- src/generate.py
+++ src/generate.py
@@ -1,6 +1,6 @@
-from random import choice
+from random import choice, randint
 
-id = [0, 1, 2, 3, 4, 5]
+id = ["HACK"]
 uzivatel = ["drahomira", "radoslav"]
 datum = [0, 1, 2, 3, 4, 5]
 url = ["google", "yahoo"]
@@ -22,3 +22,26 @@ def generate():
 
 
     return text
+
+def generate_a_file(filename, n_of_lines):
+    with open(filename, mode="w", encoding="utf-8") as f:
+        n = 0
+        while n < int(n_of_lines):
+            print(generate(), file=f)
+            n = n + 1
+
+def generate_with_file(filename, n_of_attacks):
+    f = open(filename, "r")
+    contents = f.readlines()
+    f.close()
+
+
+    n = 0
+    while n < int(n_of_attacks):
+        index = randint(0, len(contents))
+        contents.insert(index, generate())
+        n = n + 1
+
+    f = open(filename, "w")
+    f.writelines(contents)
+    f.close()
